c0fdf907a154a86e0d15df6879dd1c7ea01f6e68
Oleg <olijenius@gmail.com>
2015-08-05 10:00:31 +0300

    Fix in script sender for cron

--- application/components/console/send.php
+++ application/components/console/send.php
@@ -1,16 +1,19 @@
 <?php
 
 #Cron
-include('config');
+//include('config');
 
 $realPwd = '11';
 #if (empty($argv[1]) || $argv[1] != $realPwd) exit('Incorrect password');
 
-include('lib/mail.php');      // подключаем класс для отправки почты
-include('lib/dbsql.php');     // подключаем класс работа с базой данных
-include('lib/mailStec.php');  // подключаем наш класс отложенной рассылки
+include('../../libs/massmail/mail.php');      // подключаем класс для отправки почты
+include('../../libs/massmail/dbsql.php');     // подключаем класс работа с базой данных
+include('../../libs/massmail/mailStec.php');  // подключаем наш класс отложенной рассылки
+
+include('../../config.php');
+
 // устанавливаем подключение с БД, указав параметры доступа к БД
-$DB=new DB_Engine('mysql', DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$DB=new DB_Engine('mysql', DB_HOST, DB_USER, DB_PWD, DB_NAME);
 // создаем объект для отправки почты
 $m  = new Mail();
 $ms = new mailStec($DB, $m);// инициализация отложенной рассылки
