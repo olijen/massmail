801aea2c78afdd1505da69010d6deacb519f9e3f
root <root@dev64.stacklet.com>
2015-08-05 13:00:36 +0400

    cron script fix

--- application/libs/massmail/send.php
+++ application/libs/massmail/send.php
@@ -2,18 +2,17 @@
 
 #Cron
 
-include('/application/config.php');
+include(DR.'/application/config.php');
 
-include('/application/libs/massmail/mail.php');      // подключаем класс для отправки почты
-include('/application/libs/massmail/dbsql.php');     // подключаем класс работа с базой данных
-include('/application/libs/massmail/mailStec.php');  // подключаем наш класс отложенной рассылки
+include(DR.'/application/libs/massmail/mail.php');
+include(DR.'/application/libs/massmail/dbsql.php');
+include(DR.'/application/libs/massmail/mailStec.php');
 
-// устанавливаем подключение с БД, указав параметры доступа к БД
 $DB=new DB_Engine('mysql', DB_HOST, DB_USER, DB_PWD, DB_NAME);
-// создаем объект для отправки почты
+
 $m  = new Mail();
-$ms = new mailStec($DB, $m);// инициализация отложенной рассылки
-$ms->db = $DB; // передаем в отложенную рассылку указатель БД, чтобы скрипт отложенной рассылки мог работать с базой данных, используя методы класса mbsql.class.php
-$ms->m  = $m;  // передаем в отложенную рассылку, объект работы с почтой
-// Отправить из очереди 50 писем, если не указать кол-во будет отправлено по умолчанию 100 писем из очереди
-$ms->SendStec(50,true);
+$ms = new mailStec($DB, $m);
+$ms->db = $DB;
+$ms->m  = $m;
+
+$ms->SendStec(200, true);
--- index_cron.php
+++ index_cron.php
@@ -7,4 +7,5 @@ if (empty($argv[2])) exit('Incorrect include files');
 
 $base = $argv[2];
 
-include($base);
+define('DR', dirname(__FILE__));
+include(DR.$base);
